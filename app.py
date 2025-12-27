0cb2407c22faa02bb9207ff2e4b02a950dc25289
magomagoz <magostinienrico@gmail.com>
2025-12-27 22:26:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ def calcola_pronostico_streamlit(nome_input):
 
 # --- MAIN APP ---
 st.set_page_config(page_title="Delphi Pro", layout="wide")
-st.title("Delphi Predicto")
+st.title("Delphi Predictor")
 tab_analisi, tab_gestione = st.tabs(["🎯 Analisi Match", "⚙️ Gestione"])
 
 with tab_analisi:
