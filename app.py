e5bdc0197837e66cbb5ea848f3ac134187713af9
magomagoz <magostinienrico@gmail.com>
2025-12-28 15:07:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ import os
 import time
 from datetime import datetime
 
-st.set_page_config(page_title="Delphi Predictor Pro", layout="wide")
-
-# Banner Identitario
-st.image("Banner.png", use_container_width=True)
-st.markdown("---")
-
 # --- CONFIGURAZIONE ---
 API_TOKEN = 'c7a609a0580f4200add2751d787b3c68'
 FILE_DB = 'database_pro_2025.csv'
